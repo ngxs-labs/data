d47f5aec139f58b7283d490c70730643b4fd14a8
splincode <omaxphp@yandex.ru>
2019-12-29 19:19:20 +0300

    fix: corrected reset with children

--- src/lib/decorators/repository/data/state-repository.ts
+++ src/lib/decorators/repository/data/state-repository.ts
@@ -3,22 +3,22 @@ import { StateClass } from '@ngxs/store/internals';
 import { ensureStoreMetadata } from '@ngxs/store';
 
 import { createRepositoryMetadata } from '../utils/create-repository-metadata';
-import { NGXS_DATA_EXCEPTIONS } from '../../../interfaces/external.interface';
 import { createStateSelector } from '../utils/create-state-selector';
-import { Any } from '../../../interfaces/internal.interface';
+import { Any, NGXS_DATA_EXCEPTIONS } from '../../../interfaces/internal.interface';
 import { createContext } from '../utils/create-context';
-import { clone } from '../../../internals/utils';
+import { buildDefaultsGraph } from '../../../internals/utils';
 
 export function StateRepository(): ClassDecorator {
     return <TFunction extends Function>(stateClass: TFunction): TFunction | void => {
-        const stateMeta: MetaDataModel = ensureStoreMetadata((stateClass as Any) as StateClassInternal);
+        const stateClassInternal: StateClassInternal = (stateClass as Any) as StateClassInternal;
+        const stateMeta: MetaDataModel = ensureStoreMetadata(stateClassInternal);
 
         if (!stateMeta.name) {
             throw new Error(NGXS_DATA_EXCEPTIONS.NGXS_DATA_STATE);
         }
 
-        createRepositoryMetadata((stateClass as Any) as StateClass, stateMeta);
-        const cloneDefaults: Any = clone(stateMeta.defaults);
+        createRepositoryMetadata(stateClassInternal, stateMeta);
+        const cloneDefaults: Any = buildDefaultsGraph(stateClassInternal);
 
         Object.defineProperties(stateClass.prototype, {
             name: { enumerable: true, configurable: true, value: stateMeta.name },
--- src/lib/interfaces/internal.interface.ts
+++ src/lib/interfaces/internal.interface.ts
@@ -1,25 +1,49 @@
 import { ActionType, Store } from '@ngxs/store';
 import { Immutable, PersistenceProvider } from './external.interface';
 
+/**
+ * @privateApi
+ */
 export interface PlainObjectOf<T> {
     [key: string]: T;
 }
 
+/**
+ * @privateApi
+ */
 // tslint:disable-next-line:no-any
 export type Any = any;
 
+/**
+ * @privateApi
+ */
 export type Primitive = undefined | null | boolean | string | number | Function;
 
+/**
+ * @privateApi
+ */
 export interface DeepImmutableArray<T> extends ReadonlyArray<Immutable<T>> {}
 
+/**
+ * @privateApi
+ */
 export interface DeepImmutableMap<K, V> extends ReadonlyMap<Immutable<K>, Immutable<V>> {}
 
+/**
+ * @privateApi
+ */
 export type DeepImmutableObject<T> = {
     readonly [K in keyof T]: Immutable<T[K]>;
 };
 
+/**
+ * @privateApi
+ */
 export type ActionEvent = ActionType & { payload: PlainObjectOf<Any> };
 
+/**
+ * @privateApi
+ */
 export interface RootInternalStorageEngine {
     readonly size: number;
     readonly store: Store | null;
@@ -32,3 +56,31 @@ export interface RootInternalStorageEngine {
 
     ensureKey(provider: PersistenceProvider): string;
 }
+
+/**
+ * @privateApi
+ */
+export enum NGXS_DATA_EXCEPTIONS {
+    NGXS_PERSISTENCE_STATE = '@Persistence should be add before decorator @State and @StateRepository',
+    NGXS_DATA_STATE = '@StateRepository should be add before decorator @State',
+    NGXS_DATA_STATE_NAME_NOT_FOUND = 'State name not provided in class',
+    NGXS_DATA_MODULE_EXCEPTION = 'Metadata not created \n Maybe you forgot to import the NgxsDataPluginModule' +
+        '\n Also, you cannot use this.ctx.* until the application is fully rendered ' +
+        '\n (use by default ngxsOnInit(ctx: StateContext), or ngxsAfterBootstrap(ctx: StateContext) !!!',
+    NGXS_DATA_STATE_DECORATOR = 'You forgot add decorator @StateRepository or initialize state!' +
+        '\n Example: NgxsModule.forRoot([ .. ]), or NgxsModule.forFeature([ .. ])',
+    NGXS_DATA_STATIC_ACTION = 'Cannot support static methods with @action',
+    NGXS_DATA_ACTION = '@action can only decorate a method implementation',
+    NGXS_DATA_ACTION_RETURN_TYPE = 'RECOMMENDATION: If you use asynchronous actions' +
+        ' `@action({ async: true })`, ' +
+        'the return result type should only be Observable or void instead',
+    NGXS_PERSISTENCE_ENGINE = 'Not declare storage engine in `existingEngine` or not found after injecting by `useClass`',
+    NGXS_PERSISTENCE_SERIALIZE = 'Error occurred while serializing the store value, value not updated.',
+    NGXS_PERSISTENCE_DESERIALIZE = 'Error occurred while deserializing the store value, falling back to empty object.',
+    NGXS_DATA_CHILDREN_CONVERT = 'Child states can only be added to an object'
+}
+
+/**
+ * @privateApi
+ */
+export const NGXS_DATA_META: string = 'NGXS_DATA_META';
