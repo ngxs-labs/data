c2db134711faf029ace4094b3fbcf2568ae2554f
Max Ivanov <omaxphp@yandex.ru>
2020-02-24 10:31:11 +0300

    Update public_api.ts

--- lib/internals/src/public_api.ts
+++ lib/internals/src/public_api.ts
@@ -3,5 +3,6 @@ export * from './utils/is-not-nil';
 export * from './utils/deep-close-defaults';
 export * from './utils/is-plain-object';
 export * from './utils/get-store-options';
-// todo: need reuse
-// export * from './utils/build-defaults-graph';
+
+// if uncommented we have empty build after compile
+export * from './utils/build-defaults-graph';
