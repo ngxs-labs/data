8e7f57f84ddfe0d532ca0a64492e984e710bb93f
splincode <omaxphp@yandex.ru>
2020-05-05 20:58:17 +0300

    fix: function expression are not supported in decorators

--- lib/storage/src/tokens/storage-container-provider.ts
+++ lib/storage/src/tokens/storage-container-provider.ts
@@ -1,9 +1,9 @@
 import { Provider } from '@angular/core';
 
-import { NgxsDataStorageContainer } from '../ngxs-data-storage-container';
 import { NGXS_DATA_STORAGE_CONTAINER_TOKEN } from './storage-container-token';
+import { storageUseFactory } from './storage-use-factory';
 
 export const NGXS_DATA_STORAGE_CONTAINER: Provider = {
     provide: NGXS_DATA_STORAGE_CONTAINER_TOKEN,
-    useFactory: (): NgxsDataStorageContainer => new NgxsDataStorageContainer()
+    useFactory: storageUseFactory
 };
