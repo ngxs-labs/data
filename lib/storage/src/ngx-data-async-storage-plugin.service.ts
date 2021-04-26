09cba64fb3cc33e4f45b6e2ca796f80a3615c1e6
Ishan Udayapriya <ishanuda@gmail.com>
2021-04-26 22:37:58 +0530

    Updated comments

--- lib/storage/src/ngx-data-async-storage-plugin.service.ts
+++ lib/storage/src/ngx-data-async-storage-plugin.service.ts
@@ -10,26 +10,53 @@ export class NgxsDataAsyncStoragePlugin implements AsyncStorageEngine {
     private storage: StorageMap
   ) { }
 
+  /**
+   * Returns the size of the storage
+   */
   length(): Observable<number> {
     return this.storage.size;
   }
 
+  /**
+   * @param key: any
+   * @description:
+   * Returns an item by the given key
+   */
   getItem(key: any): Observable<any> {
     return this.storage.get(key);
   }
 
+  /**
+   * @param key: any
+   * @param val: any
+   * @description:
+   * Set value with the given key
+   */
   setItem(key: any, val: any): void {
     this.storage.set(key, val);
   }
 
+  /**
+   * @description:
+   * Removes an item by the given key
+   */
   removeItem(key: any): void {
     this.storage.delete(key);
   }
 
+  /**
+   * @description:
+   * Clear the storage
+   */
   clear(): void {
     this.storage.clear();
   }
 
+  /**
+   * @param val: number
+   * @description:
+   * Returns the key by the given value
+   */
   key(val: number): Observable<string> {
     return from(this.storage.keys().toPromise().then(keys => keys[val]));
   }
